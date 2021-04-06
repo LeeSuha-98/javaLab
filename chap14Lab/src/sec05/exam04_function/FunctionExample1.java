9ef69162ec3b5c5d42f0b27b02a1b6d5838e4ca2
LeeSuha-98 <dltngk10@naver.com>
2021-04-06 14:33:28 +0900

    chap14 예제

--- chap14Lab/src/sec05/exam04_function/FunctionExample1.java
+++ chap14Lab/src/sec05/exam04_function/FunctionExample1.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
 import java.util.function.ToIntFunction;
 
 public class FunctionExample1 {
+	//List 자료구조 = 자료를 인덱스로 관리하는 배열과 비슷하지만 확장을 할 수 있다.
 	private static List<Student> list = Arrays.asList(
 		new Student("홍길동", 90, 96),
 		new Student("신용권", 95, 93)
