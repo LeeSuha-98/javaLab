deb49bb9fd9e29672ffd9001d6e47be0a37aa9c5
LeeSuha-98 <dltngk10@naver.com>
2021-04-09 11:37:49 +0900

    chap16 예제

--- chap15Lab/src/verify/exam07/Board.java
+++ chap15Lab/src/verify/exam07/Board.java
@@ -3,12 +3,17 @@ package verify.exam07;
 public class Board {
 	private String title;
 	private String content;
-	
+
 	public Board(String title, String content) {
 		this.title = title;
 		this.content = content;
 	}
 
-	public String getTitle() { return title; }
-	public String getContent() { return content; }
+	public String getTitle() {
+		return title;
+	}
+
+	public String getContent() {
+		return content;
+	}
 }
